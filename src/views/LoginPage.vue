f813d40a8b24def59a81e8ba4a2fcca60020a8c6
realview45 <kjink0125@gmail.com>
2026-03-31 21:37:08 +0900

    href 라우팅방식 주석추가

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -46,6 +46,9 @@ export default{
             }
             const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/member/doLogin`, data);
             localStorage.setItem("token", response.data.token);
+            // 화면전체에 새로고침이 발생하는 라우팅방식.
+            // 특징1. 화면상에 모든 컴포넌트가 새롭게 created 
+            // 특징2. 화면에 깜빡임으로 인해 사용자경험이 떨어질 수 있음.
             window.location.href="/";
        }
     }
