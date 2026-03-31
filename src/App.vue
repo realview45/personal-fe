0cba10e7f24f415b8c0bc878067738ad2e3979aa
realview45 <kjink0125@gmail.com>
2026-03-31 11:39:02 +0900

    initialize

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>Hello, Vue 3!</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+</style>
