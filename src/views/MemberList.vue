7975ae9d42f37bba1b86e276877977c8ec57702b
realview45 <kjink0125@gmail.com>
2026-03-31 21:58:14 +0900

    목록테이블 생성 get요청시 token없어 403에러

--- src/views/MemberList.vue
+++ src/views/MemberList.vue
@@ -8,7 +8,24 @@
                     </v-card-title>
                     <v-card-text>
                         <v-table>
-                            
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>이름</th>
+                                    <th>이메일</th>
+                                    <th>채팅</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="member in memberList" :key="member.id">
+                                    <td>{{ member.id }}</td>
+                                    <td>{{ member.name }}</td>
+                                    <td>{{ member.email }}</td>
+                                    <td>
+                                        <v-btn>채팅하기</v-btn>
+                                    </td>
+                                </tr>
+                            </tbody>
                         </v-table>
                     </v-card-text>
                 </v-card>
@@ -17,14 +34,18 @@
     </v-container>
 </template>
 <script>
+import axios from 'axios';
+
 export default{
     data(){
         return{
-
+            memberList : []
         }
     },
-    created(){
-
+    async created(){
+        const response = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/member/list`);
+        this.memberList = response.data;
+        console.log(response);
     }
 }
 </script>
