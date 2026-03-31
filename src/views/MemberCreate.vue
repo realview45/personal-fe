d9bda947920aeee20f7e66a33e375474eef7f8da
realview45 <kjink0125@gmail.com>
2026-03-31 16:11:10 +0900

    v-form v-text-field type 회원가입 이메일 정보입력창 생성

--- src/views/MemberCreate.vue
+++ src/views/MemberCreate.vue
@@ -5,6 +5,27 @@
                 <v-card>
                     <v-card-title class="text-h5 text-center">회원가입</v-card-title>
                 </v-card>
+                <v-card-text>
+                    <v-form @submit.prevent="memberCreate">
+                        <v-text-field
+                        label="이름"
+                        v-model="name"
+                        required>
+                        </v-text-field>
+                        <v-text-field
+                        label="이메일"
+                        v-model="email"
+                        type="email"
+                        required>
+                        </v-text-field>
+                                                <v-text-field
+                        label="비밀번호"
+                        v-model="password"
+                        type="password"
+                        required>
+                        </v-text-field>
+                    </v-form>
+                </v-card-text>
             </v-col>
         </v-row>
     </v-container>
@@ -14,7 +35,9 @@
 export default{
     data(){
         return{
-
+            name:"",
+            email:"",
+            password:"",
         }
     },
     methods:{
